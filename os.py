2be471eb207a0f241576d8cfa6fb0842a7c92218
Zeyu丶泽宇 <zhang@zeyu.tw>
2022-07-07 23:15:31 -0400

    add return_output option for os.run_cmd()

--- os.py
+++ os.py
@@ -2,9 +2,12 @@ import subprocess
 import sys
 
 
-def run_cmd(cmd, failure_warning=True):
+def run_cmd(cmd, failure_warning=True, return_output=True):
     try:
-        return subprocess.check_output(cmd, shell=True, universal_newlines=True).strip("\n")
+        if return_output:
+            return subprocess.check_output(cmd, shell=True, universal_newlines=True).strip("\n")
+        else:
+            return subprocess.check_call(cmd, shell=True)
     except subprocess.CalledProcessError as e:
         if failure_warning:
             print("WARNING:", e, file=sys.stderr)
